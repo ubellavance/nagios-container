387e33e0b8ee2cbdd426458704810ed76e94b569
Steve Bosman <bosmansteve@gmail.com>
2017-04-20 09:30:20 -0600

    added start up's
    added services to start

--- entry.sh
+++ entry.sh
@@ -1,5 +1,7 @@
 # Start supporting services
 /etc/init.d/httpd start
 /etc/init.d/postfix start
+/etc/init.d/crond start
+/etc/init.d/nrpe start
 
 exec ${NAGIOS_BIN}/usr/sbin/nagios ${NAGIOS_HOME}/nagios.cfg
