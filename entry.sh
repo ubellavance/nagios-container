1c651cb8442c63621cc69669f8df58f8552e4309
Steve Bosman <bosmansteve@gmail.com>
2017-05-20 18:47:38 -0600

    fix

--- entry.sh
+++ entry.sh
@@ -1,5 +1,5 @@
 # Lets checkconfig these to always startup
-RUN for startup in nrpe crond httpd nagios sendmail;do /sbin/chkconfig $startup on;done
+for startup in nrpe crond httpd nagios sendmail; do /sbin/chkconfig $startup on; done
 
 # Configure services to star
-RUN for service in nrpe crond httpd nagios sendmail;do /sbin/service $service start;done
+for service in nrpe crond httpd nagios sendmail; do /sbin/service $service start; done
